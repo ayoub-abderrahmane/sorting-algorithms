255cc259b9741f40c124fb65c5c99a7bd64d73cb
Ayoub Abderrahmane <ayoub-abderrahmane@laplateforme.io>
2024-04-03 10:08:53 +0200

    Programme qui permet toujours de trier une liste de 5 nombres mais remplaçement des 'if-elif' par une méthode 'check_numbers'

--- insertion.py
+++ insertion.py
@@ -2,21 +2,30 @@ class Insertion():
     def __init__(self, liste):
         self.list = liste
     
+    def check_numbers(self, first_value, second_value):
+        if self.list[first_value] > self.list[second_value]:
+            self.list [first_value] ,self.list[second_value] = self.list[second_value] ,self.list[first_value]
+
     def sorting(self):
         print ("Initial list : " ,self.list)
         for index ,numbers in enumerate(self.list):
             for i in range(len(self.list)):
-                if self.list[0] > self.list[1]:
-                    self.list [0] ,self.list[1] = self.list[1] ,self.list[0]
-                elif self.list[1] > self.list[2]:
-                    self.list [1] ,self.list[2] = self.list[2] ,self.list[1]
-                elif self.list[2] > self.list[3]:
-                    self.list [2] ,self.list[3] = self.list[3] ,self.list[2]
-                elif self.list[3] > self.list[4]:
-                    self.list [3] ,self.list[4] = self.list[4] ,self.list[3]
+                self.check_numbers(0,1)
+                self.check_numbers(1,2)
+                self.check_numbers(2,3)
+                self.check_numbers(3,4)
+                # if self.list[0] > self.list[1]:
+                #     self.list [0] ,self.list[1] = self.list[1] ,self.list[0]
+                # elif self.list[1] > self.list[2]:
+                #     self.list [1] ,self.list[2] = self.list[2] ,self.list[1]
+                # if self.list[2] > self.list[3]:
+                #     self.list [2] ,self.list[3] = self.list[3] ,self.list[2]
+                # elif self.list[3] > self.list[4]:
+                #     self.list [3] ,self.list[4] = self.list[4] ,self.list[3]
         print(self.list)
 
 
 
-insert = Insertion([5,4,3,2,1])
+insert = Insertion([5,2,3,1,4])
+# insert.check_numbers()
 insert.sorting()
