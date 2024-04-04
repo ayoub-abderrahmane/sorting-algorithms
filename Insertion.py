4f6ba122e2d6a55d6b50e65f17b765380ad98ecd
Ayoub Abderrahmane <ayoub-abderrahmane@laplateforme.io>
2024-04-04 11:56:25 +0200

    test dans la méthode check_numbers

--- Insertion.py
+++ Insertion.py
@@ -2,31 +2,18 @@ class Insertion():
     def __init__(self, liste):
         self.list = liste
     
-    def check_numbers(self, first_value, second_value):
-        if self.list[first_value] > self.list[second_value]:
-            self.list [first_value] ,self.list[second_value] = self.list[second_value] ,self.list[first_value]
+    def check_numbers(self, first_value, temporary_key):
+        if self.liste[first_value] < self.liste[temporary_key]:
+            self.list[second_value]
 
     def sorting(self):
         print ("Initial list : " ,self.list)
-        for index ,numbers in enumerate(self.list):
-            self.check_numbers(0,1)
-            # index += 1
-            # print(index)
-            self.check_numbers(1,2)
-            self.check_numbers(2,3)
-            self.check_numbers(3,4)
-                # if self.list[0] > self.list[1]:
-                #     self.list [0] ,self.list[1] = self.list[1] ,self.list[0]
-                # elif self.list[1] > self.list[2]:
-                #     self.list [1] ,self.list[2] = self.list[2] ,self.list[1]
-                # if self.list[2] > self.list[3]:
-                #     self.list [2] ,self.list[3] = self.list[3] ,self.list[2]
-                # elif self.list[3] > self.list[4]:
-                #     self.list [3] ,self.list[4] = self.list[4] ,self.list[3]
-        print(self.list)
+        for index, numbers in enumerate(self.list):
+            for i in range(len(self.list)):
+                pass
+        # print(self.list)
 
 
 
 insert = Insertion([5,3,2,1,4])
-# insert.check_numbers()
 insert.sorting()
