cb24bff80522f57daab22bc6123982608f7b033a
Ayoub Abderrahmane <ayoub-abderrahmane@laplateforme.io>
2024-04-08 10:42:51 +0200

    optimisation du code

--- Bulles.py
+++ Bulles.py
@@ -1,32 +1,23 @@
 class Bulles():
     def __init__(self, liste):
-        self.list = liste
+        self.list = liste # Initialization of the list
+        self.len_list = len(self.list) # Take the len of the list
     
-    def check_numbers(self, first_value, second_value):
-        if self.list[first_value] > self.list[second_value]:
-            self.list [first_value] ,self.list[second_value] = self.list[second_value] ,self.list[first_value]
-
     def sorting(self):
-        print ("Initial list : " ,self.list)
-        for index ,numbers in enumerate(self.list):
-            self.check_numbers(0,1)
-            # index += 1
-            # print(index)
-            self.check_numbers(1,2)
-            self.check_numbers(2,3)
-            self.check_numbers(3,4)
-                # if self.list[0] > self.list[1]:
-                #     self.list [0] ,self.list[1] = self.list[1] ,self.list[0]
-                # elif self.list[1] > self.list[2]:
-                #     self.list [1] ,self.list[2] = self.list[2] ,self.list[1]
-                # if self.list[2] > self.list[3]:
-                #     self.list [2] ,self.list[3] = self.list[3] ,self.list[2]
-                # elif self.list[3] > self.list[4]:
-                #     self.list [3] ,self.list[4] = self.list[4] ,self.list[3]
+        print ("Initial list : " ,self.list) # Print the list before sorting
+        verif = True
+        j = 0
+        while verif:
+            verif = False
+            j += 1
+            for i in range(0, (self.len_list) - j):
+                if self.list[i] > self.list[i+1]:
+                    self.list[i], self.list[i+1] = self.list[i+1], self.list[i]
+                    verif = True
+
         print(self.list)
 
 
 
-bulles = Bulles([5,3,2,1,4])
-# insert.check_numbers()
+bulles = Bulles([5,3,2,1,4,9,6,10,8,7])
 bulles.sorting()
