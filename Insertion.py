e4132bc108ee34a2d4b053ab9572991d5082fb2f
Ayoub Abderrahmane <ayoub-abderrahmane@laplateforme.io>
2024-04-11 15:36:39 +0200

    Changement des fichier insertion et bulle pour supprimer les classes

--- Insertion.py
+++ Insertion.py
@@ -1,21 +1,13 @@
-class Insertion():
-    def __init__(self, liste):
-        self.list = liste
-        self.len_list = len(self.list)
-    
+def insertion_sort(liste):
+    len_list = len(liste)
+    for i in range(len_list):
+        key = liste[i]
+        j = i - 1
+        while j >= 0 and liste[j] > key:
+            liste[j + 1] = liste[j]
+            j -= 1
+            liste[j + 1] = key
+    return liste
 
-    def sorting(self):
-        # print ("Initial list : " ,self.list)
-        for i in range(self.len_list):
-            key = self.list[i]
-            j = i - 1
-            while j >= 0 and self.list[j] > key:
-                self.list[j+1] = self.list[j]
-                j -= 1
-                self.list[j+1] = key
-        return self.list
-
-
-
-insert = Insertion([5,3,2,1,4,9,6,8,10,7])
-insert.sorting()
+ma_liste = [5, 3, 2, 1, 4, 9, 6, 8, 10, 7]
+insertion_sort(ma_liste)
--- Peigne.py
+++ Peigne.py
@@ -1,30 +1,23 @@
-class Peigne:
-    def __init__(self, liste):
-        self.list = liste
-        self.len_list = len(self.list)
+def comb_sort(liste):
+    len_list = len(liste)
+    gap = len_list
+    shrink = 1.3
+    sorted = False
 
-    def comb_sort(self):
-        gap = self.len_list
-        shrink = 1.3
-        sorted = False
+    while not sorted:
+        gap = int(gap / shrink)
+        if gap <= 1:
+            gap = 1
+            sorted = True
 
-        while not sorted:
-            # Update the gap value for a next comb
-            gap = int(gap / shrink)
-            if gap <= 1:
-                gap = 1
-                sorted = True  # If there are no swaps, the list is sorted
+        i = 0
+        while i + gap < len_list:
+            if liste[i] > liste[i + gap]:
+                liste[i], liste[i + gap] = liste[i + gap], liste[i]
+                sorted = False
+            i += 1
 
-            i = 0
-            while i + gap < self.len_list:
-                if self.list[i] > self.list[i + gap]:
-                    # Swap elements at positions i and i+gap
-                    self.list[i], self.list[i + gap] = self.list[i + gap], self.list[i]
-                    sorted = False  # If a swap occurs, the list is not fully sorted
-                i += 1
+    return liste
 
-        return self.list
-
-
-pei = Peigne([5, 3, 2, 1, 4, 8, 6, 9, 7, 10])
-print (pei.comb_sort())
+ma_liste = [5, 3, 2, 1, 4, 8, 6, 9, 7, 10]
+print(comb_sort(ma_liste))
