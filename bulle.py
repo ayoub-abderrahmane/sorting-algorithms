026c79850f857887bdb2d27b6609ca83026bdebd
ilyes-chabab <ilyes.chabab@laplateforme.io>
2024-04-11 16:09:40 +0200

    ajout de commentaire sur le main

--- bulle.py
+++ bulle.py
@@ -12,8 +12,3 @@ def tri_bulle(tableau):
                 tableau[en_cours + 1],tableau[en_cours]
     return tableau
 
-# Exemple d'utilisation
-liste_a_trier = [3, 7, 2, 1, 9, 5]
-print("Avant le tri :", liste_a_trier)
-liste_triee = tri_bulle(liste_a_trier)
-print("Après le tri :", liste_triee)
--- test.py
+++ test.py
@@ -6,18 +6,23 @@ import random
 import time
 
 
-# Initialisation de Pygame
+# pygame init
 pygame.init()
 
-# Définition des couleurs
-
-# Dimensions de la fenêtre
+#widht and height of the window
 WINDOW_WIDTH = 1250
 WINDOW_HEIGHT = 600
 
-# Création de la fenêtre
+# create the window
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+#page name
 pygame.display.set_caption("Tri du spectre de couleur")
+
+"""
+function to convert hsv to rgb because hsv has 1 argument for color(the 2 other are for the saturation and brightness , 
+it's not important)
+and rgb has 3 argument. So more rgb is harder to associate a number to a color than hsv.
+"""
 
 def hsv_to_rgb(h):
     """
@@ -26,13 +31,10 @@ def hsv_to_rgb(h):
     """
     r, g, b = colorsys.hsv_to_rgb(h / 360.0, 1, 1)
     return int(r * 255), int(g * 255), int(b * 255)
-
-h = 0 
-
 # print("Couleur RGB:", hsv_to_rgb(h))
 
-def color_circle_without_sort(xi,yi):
-    colors = list(range(361))
+def color_rectangle_without_sort(xi,yi,element_num):
+    colors = list(range(element_num+1))
     random.shuffle(colors)
     y=300
     x= xi+50
@@ -56,9 +58,10 @@ def color_circle_without_sort(xi,yi):
             y+=1
             x+=1
 
-def color_circle(xi,yi,sort):
+def color_rectangle(xi,yi,sort,element_num):
+    total_time=0
     starting=time.time()
-    colors = list(range(361))
+    colors = list(range(element_num+1))
     random.shuffle(colors)
     y=300
     x= xi+50
@@ -81,13 +84,10 @@ def color_circle(xi,yi,sort):
         elif 360/4*2 < i < 360/4*3:
             y-=1
             x+=1
-        elif 360/4*3 < i < 360/4*4:
-            
+
+        elif 360/4*3 < i < 360/4*4:         
             y+=1
             x+=1
-    ending=time.time()
-    total_time= (ending - starting)
-    print(sort,":",total_time)
     
 
 def message(size,message,message_rectangle,color):
@@ -95,51 +95,55 @@ def message(size,message,message_rectangle,color):
     message = font.render(message,False,color)
     window.blit(message,message_rectangle) 
 # Initialisation du cercle affiché
-circle_displayed = False
 
-# Boucle principale
-running = True
-sorting=[SelectionSort,MergeSort,QuikSort,bubble_sort,heap_sort]
-sortingMessage=["SelectionSort","MergeSort","QuikSort"]
-total_time=0
-while running:
-    # Gestion des événements
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RETURN:
-                # Inversion de l'état du cercle affiché
-                circle_displayed = not circle_displayed
-                if circle_displayed:  
-                    yi=300
-                    xi=120
-                    for i in sorting:
-                        color_circle_without_sort(xi,yi)
-                        xi+=180
-                else:
-                    yi=300
-                    xi=120
-                    m=0
-                    for i in sorting:
-                        color_circle(xi,yi,i)
-                        message(24,f"{sortingMessage[m]}",(xi-75,yi+100,0,0),(255,255,255))
-                        message(24,f"{total_time}",(xi-75,yi+135,0,0),(255,255,255))
-                        xi+=180
-                        m+=1
-                        
-                        
-                
-    # Mise à jour de l'affichage
-    pygame.display.flip()
-    
-    # Limiter le taux de rafraîchissement
-    pygame.time.Clock().tick(60)
 
-# Quitter Pygame
-pygame.quit()
-sys.exit()
+def main():
+    rectangle_displayed = False
+    running = True
+    sorting=[SelectionSort,MergeSort,QuikSort,bubble_sort,heap_sort]
+    sortingMessage=["SelectionSort","MergeSort","QuikSort","bubble_sort","heap_sort"]
+    total_time=0
+    element_number=360
+    while running:
+        # Gestion des événements
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    #display rectangle not sorted
+                    message(24,f"elements:{element_number}",(0,0,0,0),(255,255,255))
+                    rectangle_displayed = not rectangle_displayed
+                    if rectangle_displayed:  
+                        yi=300
+                        xi=120
+                        for i in sorting:
+                            color_rectangle_without_sort(xi,yi,element_number)
+                            xi+=180
+                    else:
+                        yi=300
+                        xi=120
+                        m=0
+                        for i in sorting:
+                            starting=time.time()
+                            color_rectangle(xi,yi,i,element_number)
+                            ending=time.time()
+                            total_time=ending-starting
+                            total_time = "{:.2f}".format(total_time)
+                            message(24,f"{total_time}s",(xi-75,yi+135,0,0),(255,255,255))
+                            message(24,f"{sortingMessage[m]}",(xi-75,yi+100,0,0),(255,255,255))
+
+                            xi+=180
+                            m+=1
+                            
+        pygame.display.flip()
+        pygame.time.Clock().tick(60)
+
+if __name__ == "__main__":
+    main()
+
+
 
 
 
