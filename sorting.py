935135a5c7150658c62e78a8b6eb3d4405f50b52
ilyes-chabab <ilyes.chabab@laplateforme.io>
2024-04-11 12:01:09 +0200

    merge des fonctions de mehdi

--- sorting.py
+++ sorting.py
@@ -52,3 +52,45 @@ def SelectionSort(list):
 
             selection = [list[0]]
     return list
+
+#4. BubbleSort
+def bubble_sort(array):
+    swapped = True
+    pass_num = 0
+    while swapped:
+        swapped = False
+        pass_num += 1
+        for current_index in range(0, len(array) - pass_num):
+            if array[current_index] > array[current_index + 1]:
+                swapped = True
+                # Swap the two elements
+                array[current_index], array[current_index + 1] = \
+                array[current_index + 1], array[current_index]
+    return array
+#5.heap_sort
+def heap_sort(array):
+    def heapify(array, n, i):
+        largest = i
+        left = 2 * i + 1
+        right = 2 * i + 2
+
+        if left < n and array[i] < array[left]:
+            largest = left
+
+        if right < n and array[largest] < array[right]:
+            largest = right
+
+        if largest != i:
+            array[i], array[largest] = array[largest], array[i]
+            heapify(array, n, largest)
+
+    n = len(array)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(array, n, i)
+
+    for i in range(n - 1, 0, -1):
+        array[i], array[0] = array[0], array[i]
+        heapify(array, i, 0)
+
+    return array
