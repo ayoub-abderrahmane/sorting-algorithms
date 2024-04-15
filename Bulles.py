96d1acd61930f2bb09b25de334b92db4bb9ec096
Ayoub Abderrahmane <ayoub-abderrahmane@laplateforme.io>
2024-04-15 10:38:02 +0200

    modification du fichier 'Bulles.py'

--- Bulles.py
+++ Bulles.py
@@ -1,23 +1,18 @@
-class Bulles():
-    def __init__(self, liste):
-        self.list = liste # Initialization of the list
-        self.len_list = len(self.list) # Take the len of the list
+def bulles_sort(liste):
+    len_list = len(liste) # Prendre la longueur de la liste
     
-    def sorting(self):
-        # print ("Initial list : " ,self.list) # Print the list before sorting
-        verif = True
-        j = 0
-        while verif:
-            verif = False
-            j += 1
-            for i in range(0, (self.len_list) - j):
-                if self.list[i] > self.list[i+1]:
-                    self.list[i], self.list[i+1] = self.list[i+1], self.list[i]
-                    verif = True
+    
+    verif = True
+    j = 0
+    while verif:
+        verif = False
+        j += 1
+        for i in range(0, len_list - j):
+            if liste[i] > liste[i+1]: # Compare le premier élément avec le second élément
+                liste[i], liste[i+1] = liste[i+1], liste[i] # Échanger les éléments
+                verif = True # Si un échange est effectué, le tri n'est pas encore terminé
 
-        return self.list
+    return liste # renvoie la liste trié
 
-
-
-bulles = Bulles([5,3,2,1,4,9,6,10,8,7])
-bulles.sorting()
+resultat = bulles_sort([5,3,2,1,4,9,6,10,8,7])
+print(resultat)
